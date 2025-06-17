228cfea600ee9f57d7f53fec65104b27c114757a
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-06-17 21:00:50 +0300

    Prepare the app for pagination.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,22 +26,26 @@
 @extends('layouts.front')
 
 @section('content')
-  {{-- looping over this <div> --}}
-  <div class="post-preview">
-    <a href="post.html">
-      <h2 class="post-title">
-        Man must explore, and this is exploration at its greatest
-      </h2>
-      <h3 class="post-subtitle">
-        Problems look mighty small from 150 miles up
-      </h3>
-    </a>
-    <p class="post-meta">Posted by
-      <a href="#">Start Bootstrap</a>
-      on September 24, 2019
-    </p>
-  </div>
-  <hr>
+  {{-- Looping over this <div> --}}
+  @foreach ($posts as $post)
+    <div class="post-preview">
+      <a href="post.html">
+        <h2 class="post-title">
+          {{ $post->title }}
+        </h2>
+        <h3 class="post-subtitle">
+          {{-- {{ $post->body }} --}}
+          {{ Illuminate\Support\Str::words($post->body, 2, '...') }}
+        </h3>
+      </a>
+      <p class="post-meta">Posted by
+        <a href="#">{{ $post->user->name }}</a>
+        {{-- on {{ Carbon\Carbon::parse($post->created_at)->format('m-Y') }} --}}
+        on {{ $post->created_at->format('m-Y') }}
+      </p>
+    </div>
+    <hr>
+  @endforeach
 
   {{-- <!-- Pager --> --}}
   <div class="clearfix">
